c7738161040457b4b85e407236e72543ff4a4ffc
Sharvil Kekre <sharvilkekre@gmail.com>
2020-04-26 08:49:35 -0700

    query API

---
+++ query/query.py
@@ -0,0 +1,12 @@
+class Query(object):
+
+    def __init__(self,category, s):
+        self.category = category
+        self.string = s
+        self.conf = None
+        self.region = None
+
+
+# Method to process query 
+def process(query):
+    # TODO
