60281592c8e38179d8457cb9a59eac18a78d85e7
Sharvil Kekre <sharvilkekre@gmail.com>
2020-05-06 15:49:08 -0700

    converted server to flask

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+app = Flask(__name__)
+
+chatbot = ChatBot("SHAq")
+trainer = ChatterBotCorpusTrainer(chatbot)
+trainer.train("chatterbot.corpus.english")
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return str(chatbot.get_response(userText))
+
+if __name__ == "__main__":
+    app.run()
