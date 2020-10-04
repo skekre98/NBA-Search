86e9d395b9e5a1df4df2142984aeeb2a5f55c1cd
Sharvil Kekre <sharvilkekre@gmail.com>
2020-10-03 17:16:03 -0700

    .

--- visualize.py
+++ visualize.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from data.text_data import total_stat_map, adv_stat_map
 
 """
 Function to print query dataset
@@ -14,14 +15,21 @@ n/a
 def read_query_csv():
     df = pd.read_csv("data/query.csv")
     df.columns = ['Query', 'Class']
-    print("Ranking Questions:", (df.Class == 1.0).sum())
-    print("Stat Questions:", (df.Class == 2.0).sum())
+    print("Ranking Questions:", (df.Class == "rank").sum())
+    print("Stat Questions:", (df.Class == "stat").sum())
+
+def read_stat_maps():
+    print("Total:", total_stat_map)
+    print()
+    print("Advanced:", adv_stat_map)
 
 def main():
 
-    print()
     print("Reading query dataset...")
     read_query_csv()
+    print()
+    print("Reading stat maps...")
+    read_stat_maps()
 
 if __name__ == "__main__":
     main()
