4655825dc026952b44fca7887ad4f9ed97aa29ed
Sharvil Kekre <sharvilkekre@gmail.com>
2020-10-14 17:43:01 -0700

    kmeans

--- inference/cluster_store.py
+++ inference/cluster_store.py
@@ -8,4 +8,8 @@ class ClusterStore(object):
         self.clustered = False
         self.avg_similarity = 0.0
         self.avg_distance = 0.0
-        
+    
+    # Function to run KMeans clustering on data 
+    def build_kmeans_clusters(self):
+        # TODO
+        pass
