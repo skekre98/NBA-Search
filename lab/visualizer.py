a281d399445705656a33862dbf5cee584aa5f58a
Sharvil Kekre <sharvilkekre@gmail.com>
2020-10-15 17:40:15 -0700

    .

--- lab/visualizer.py
+++ lab/visualizer.py
@@ -5,5 +5,10 @@ class Visualizer(object):
     def __init__(self):
         self.plot = plt
 
+    # Function to clear current figure in plot 
+    def clear(self):
+        self.plot.clf()
+
+    # Function to display current plot 
     def display():
         self.plot.show()
