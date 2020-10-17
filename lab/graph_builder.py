fc0103cc5076fe07407a2c72632da309ae53a854
Sharvil Kekre <sharvilkekre@gmail.com>
2020-10-17 16:56:44 -0700

    .

--- lab/graph_builder.py
+++ lab/graph_builder.py
@@ -6,10 +6,15 @@ class GraphBuilder(Visualizer):
 
     def __init__(self, xlabels):
         super().__init__()
+
+        # Line Graph 
         self.lines = []
         self.line_labels = []
         self.xlabels = xlabels
         self.marker = itertools.cycle(('+', '.', 'o', '*', '1', '2', '3', '4'))
+
+        # Bar Graph 
+        self.bars = []
 
     # Function to add line to current list of lines 
     def add_line(self, line, label):
@@ -25,3 +30,11 @@ class GraphBuilder(Visualizer):
         for i, line in enumerate(self.lines):
             self.plt.plot(x, line, label=self.line_labels[i], marker=next(self.marker))
         self.plt.legend()
+    
+    # Function to add bar label/val to list of bars 
+    def add_bar(self, label, val):
+        self.bars.append((label, val))
+    
+    def build_bar_graph(self):
+        # TODO
+        pass
