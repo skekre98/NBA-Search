77bad854fc4bd0199b509c8e4d896b645c200ee1
Samin Batra <saminbatra1@gmail.com>
2020-10-20 20:00:02 +0530

    Made suggested changes

--- lab/table_builder.py
+++ lab/table_builder.py
@@ -2,23 +2,18 @@ from visualizer import Visualizer
 
 class TableBuilder(Visualizer):
 
-    def __init__(self,  columns_labels):
+    def __init__(self,  columns_labels,cell_txt):
         super().__init__()
         self.rows = []
         self.column_labels = columns_labels
-        
+        self.cell_txt = []
 
     # This function is to build a table with 
     # the rows and column labels
     def build_table(self):
         # TODO
         try:
-            visualize = Visualizer()
-            cell_txt = []
-            for row in self.rows:
-                cell_txt.append([f'{cell}' for cell in row])
-            visualize.plt.table(cell_txt,cellLoc='center',colLabels=self.column_labels)
-            visualize.display()
+            self.plt.table(self.cell_txt,cellLoc='center',colLabels=self.column_labels)
             # pass
         except Exception:
             print('An error occurred while generating the table.')
@@ -29,3 +24,4 @@ class TableBuilder(Visualizer):
             raise  Exception('Number of columns is not exact')
         else:
             self.rows.append(row)
+            self.cell_txt.append([f'{cell}' for cell in row])
