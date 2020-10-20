086ceda4226c465e7fc691b8305d94845760e9d8
Sharvil Kekre <sharvilkekre@gmail.com>
2020-10-19 22:09:15 -0700

    removed non-NBA filter

--- modules/query.py
+++ modules/query.py
@@ -1,5 +1,3 @@
-from modules.analysis import isNBA
-from data.text_data import unsure, non_nba
 from inference.inference_network import InferenceNetwork
 
 class Query(object):
@@ -9,11 +7,5 @@ class Query(object):
 
     # Method to process query 
     def process(self):
-        flag = isNBA(self.text)
-        if flag == -1:
-            return non_nba
-        elif flag == 0:
-            return unsure
-        else:
-            network = InferenceNetwork(self.text)
-            return network.response()
+        network = InferenceNetwork(self.text)
+        return network.response()
