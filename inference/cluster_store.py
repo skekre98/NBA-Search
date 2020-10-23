c4a9e936789e3f4e1ebb87d11082b164d5650da3
RaihanSabique <htraihan@gmail.com>
2020-10-24 03:52:35 +0600

    k-means clustering implemented

--- inference/cluster_store.py
+++ inference/cluster_store.py
@@ -1,4 +1,8 @@
 # Cluster API
+from sklearn.cluster import KMeans
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+
 
 class ClusterStore(object):
 
@@ -12,7 +16,39 @@ class ClusterStore(object):
     # Function to run kmeans clustering on data 
     def build_kmeans_clusters(self):
         # TODO
-        pass
+        X = self.clusters
+        mms = MinMaxScaler()
+        mms.fit(X)
+        data_transformed = mms.transform(X)
+        # print(data_transformed)
+        wcss = []
+        prev_inertia = 0
+        cur_inertia = 0
+        for i in range(1,len(data_transformed)):
+            kmeans = KMeans(n_clusters=i,init='k-means++', max_iter=300, n_init=10, random_state=0)
+            kmeans.fit(data_transformed)
+            if(i>1):
+                prev_inertia = wcss[-1]
+            cur_inertia = kmeans.inertia_
+            wcss.append(cur_inertia)
+            if(abs(prev_inertia-cur_inertia) < 1):
+                break
+        print(wcss)
+        print('Total clusters:', i)
+        total_cluster = i
+        y_kmeans = kmeans.predict(data_transformed)
+        print(y_kmeans)
+        '''
+        cluster entity map
+        storing same cluster entity in a dictionary
+        ex: cluster0: [entity1, entity10, entity50......]
+        '''
+        clusters = {}
+        for i in range(total_cluster):
+            clusters[i] = []
+        for i in range(len(y_kmeans)):
+            clusters[y_kmeans[i]].append(i)
+        print(clusters)
 
     # Function to run agglomerative clustering on current data 
     def build_agglomerative_clusters(self):
@@ -33,3 +69,13 @@ class ClusterStore(object):
     def reset(self):
         # TODO
         pass
+
+if __name__=='__main__':
+    data = np.random.uniform(0,500, [200,3])
+    # print(data)
+    entity_map ={}
+    for i in range(200):
+        entity_map[i] = 'entity'+str(i+1)
+    print(entity_map)
+    c_ob= ClusterStore(data,entity_map)
+    c_ob.build_kmeans_clusters()
