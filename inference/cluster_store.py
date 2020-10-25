6f74b82a03d07ae94cf36d974bd594036b358751
RaihanSabique <htraihan@gmail.com>
2020-10-25 20:13:58 +0600

    avg_distance measure and k-means clustering done

--- inference/cluster_store.py
+++ inference/cluster_store.py
@@ -10,16 +10,22 @@ from sklearn.preprocessing import MinMaxScaler
 class ClusterStore(object):
 
     def __init__(self, data, entity_map):
-        self.clusters = data
+        self.original_data = data
         self.entity_map = entity_map
+        self.clusters = None
+        self.total_cluster = 0
         self.clustered = False
         self.avg_similarity = 0.0
         self.avg_distance = 0.0
     
     # Function to run kmeans clustering on data 
     def build_kmeans_clusters(self):
-        # TODO
-        X = self.clusters
+        '''
+        Build optimal k-means clusters based on minimum WCSS (Maximize distance) on clusters
+        :store: Total number of cluster, Clusters data
+        :update: clustered flag
+        '''
+        X = self.original_data
         mms = MinMaxScaler()
         mms.fit(X)
         data_transformed = mms.transform(X)
@@ -34,25 +40,51 @@ class ClusterStore(object):
                 prev_inertia = wcss[-1]
             cur_inertia = kmeans.inertia_
             wcss.append(cur_inertia)
-            if(abs(prev_inertia-cur_inertia) < 1):
+            if(abs(prev_inertia-cur_inertia) < 1):   # inertia diff checking
                 break
-        print(wcss)
-        print('Total clusters:', i)
+        # print(wcss)
+        # print('Total clusters:', i)
         self.total_cluster = i
         y_kmeans = kmeans.predict(data_transformed)
-        print(y_kmeans)
-        '''
-        cluster entity map
-        storing same cluster entity in a dictionary
-        ex: cluster0: [entity1, entity10, entity50......]
-        '''
-        self.original_data = self.clusters
-        self.kmeans_clusters = {}
+        # print(y_kmeans)
+        ######################################################
+        # cluster entity map
+        # storing same cluster entity in a dictionary
+        # ex: cluster0: [entity1, entity10, entity50......]
+        #######################################################
+        self.clusters = {}
         for i in range(self.total_cluster):
-            self.kmeans_clusters[i] = []
+            self.clusters[i] = []
         for i in range(len(y_kmeans)):
-            self.kmeans_clusters[y_kmeans[i]].append(i)
-        print(self.kmeans_clusters)
+            self.clusters[y_kmeans[i]].append(i)
+        # print(self.clusters)
+        self.clustered = True
+
+    def build_kmeans_clusters_on_given_number_of_cluster(self, k):
+        '''
+        :param k: number of cluster
+        :store: Total number of cluster, Clusters data
+        :update: clustered flag
+        '''
+        X = self.original_data
+        mms = MinMaxScaler()
+        mms.fit(X)
+        data_transformed = mms.transform(X)
+        kmeans = KMeans(n_clusters=k, init='k-means++', max_iter=300, n_init=10, random_state=0)
+        kmeans.fit(data_transformed)
+        self.total_cluster = k
+        y_kmeans = kmeans.predict(data_transformed)
+        ######################################################
+        # cluster entity map
+        # storing same cluster entity in a dictionary
+        # ex: cluster0: [entity1, entity10, entity50......]
+        #######################################################
+        self.clusters = {}
+        for i in range(self.total_cluster):
+            self.clusters[i] = []
+        for i in range(len(y_kmeans)):
+            self.clusters[y_kmeans[i]].append(i)
+        # print(self.clusters)
         self.clustered = True
 
     # Function to run agglomerative clustering on current data 
@@ -69,10 +101,10 @@ class ClusterStore(object):
         '''
         :param entity1: X
         :param entity2: Y
-        :return: euclidian distance between two entity point (3D)
+        :return: euclidian distance between two entity vector (3D) X and Y
         '''
-        X = self.clusters[entity1]
-        Y = self.clusters[entity2]
+        X = self.original_data[entity1]
+        Y = self.original_data[entity2]
         return math.sqrt(pow((X[0]-Y[0]),2) + pow((X[1]-Y[1]),2) + pow((X[2]-Y[2]),2))
 
     def avg_distance_between_two_cluster(self,k1,k2):
@@ -97,12 +129,36 @@ class ClusterStore(object):
         dist = []
         for i in range(self.total_cluster):
             for j in range(i+1, self.total_cluster):
-                dist.append(self.avg_distance_between_two_cluster(self.kmeans_clusters[i],self.kmeans_clusters[j]))
+                dist.append(self.avg_distance_between_two_cluster(self.clusters[i],self.clusters[j]))
         self.avg_distance = statistics.mean(dist)
+
+    # map cluster with entity name and return the clusters
+    def get_clusters_on_entity_map(self):
+        return_ = self.clusters
+        for cluster in range(self.total_cluster):
+            entitys = [self.entity_map[x] for x in self.clusters[cluster]]
+            return_[cluster]= entitys
+            # return_[cluster].replace()
+        return return_
+
+    # map cluster with data and return the clusters
+    def get_clusters_on_data(self):
+        return_ = self.clusters
+        for cluster in range(self.total_cluster):
+            entitys = [self.original_data[x] for x in self.clusters[cluster]]
+            return_[cluster] = entitys
+            # return_[cluster].replace()
+        return return_
     # Function to reset clusters back to original data 
     def reset(self):
-        # TODO
-        pass
+        '''
+        :reset: clustered flag, total_cluster, avg_distance
+        :delete: clusters data
+        '''
+        self.clustered = False
+        self.total_cluster = 0
+        self.clusters = None
+        self.avg_distance = 0.0
 
 if __name__=='__main__':
     data = np.random.uniform(0,500, [200,3])
@@ -114,4 +170,15 @@ if __name__=='__main__':
     c_ob= ClusterStore(data,entity_map)
     c_ob.build_kmeans_clusters()
     c_ob.average_distance()
-    print(c_ob.avg_distance)
+    print('Total Clusters: ', c_ob.total_cluster)
+    print(c_ob.get_clusters_on_entity_map())
+    print('Avg distance: ',c_ob.avg_distance)
+    c_ob.reset()
+    print('Total Clusters: ', c_ob.total_cluster)
+    print(c_ob.get_clusters_on_entity_map())
+    print('Avg distance: ', c_ob.avg_distance)
+    c_ob.build_kmeans_clusters_on_given_number_of_cluster(8)
+    c_ob.average_distance()
+    print('Total Clusters: ', c_ob.total_cluster)
+    print(c_ob.get_clusters_on_entity_map())
+    print('Avg distance: ', c_ob.avg_distance)
