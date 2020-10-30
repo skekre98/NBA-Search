44e4b09609f09a5c799a59c997f37c47ced46a50
Ervin Python <44205425+Ervin66@users.noreply.github.com>
2020-10-30 18:39:39 +0100

    Added function generate_random_response
    response now returns with function call for cleaner code

--- inference/statnode.py
+++ inference/statnode.py
@@ -10,44 +10,62 @@ inc_stat += "Can you really blame me? English is not my first language..."
 
 class StatNode(object):
 
-	def __init__(self):
-		self.query = ""
-		self.nlp = spacy.load("en_core_web_md")
+    def __init__(self):
+        self.query = ""
+        self.nlp = spacy.load("en_core_web_md")
 
-	def load_query(self, query):
-		self.query = query
+    def load_query(self, query):
+        self.query = query
 
-	def response(self):
-		name = self.extract_name()
-		stat = self.extract_stat()
-		if not name:
-			return inc_name
-		elif not stat:
-			return inc_stat
-		
-		stat_val = self.get_player_stat(name, stat)
-		resp_1 = "Seems {} has a {} of {}.".format(name, stat, stat_val)
-		resp_2 = "After checking my little black book, I've found that {} has {} under his name for {}.".format(name, stat_val, stat)
-		resp_3 = "{}...obviously".format(stat_val)
-		resp_4 = "Last I checked... {} had a {} of {}.".format(name, stat, stat_val)
-		resp_5 = "Do you not have every NBA stat since the beginning of time memorized? Well he has a {} of {}.".format(stat, stat_val)
-		resp_list = [resp_1, resp_2, resp_3, resp_4, resp_5]
-		return random.choice(resp_list)
-	
-	def extract_name(self):
-		doc = self.nlp(self.query)
-		name = None
-		for entity in doc.ents:
-			if entity.label_ == "PERSON":
-				name = entity.text
-		if not name:
-			for entity in doc.ents:
-				if entity.label_ == "ORG":
-					name = entity.text
+    def generate_random_response(self):
+        name = self.extract_name()
+        stat = self.extract_stat()
+        if not name:
+            return inc_name
+        elif not stat:
+            return inc_stat
 
-		return name
-	
-	def extract_stat(self):
+        stat_val = self.get_player_stat(name, stat)
+        resp_1 = "Seems {} has a {} of {}.".format(name, stat, stat_val)
+        resp_2 = "After checking my little black book, I've found that {} has {} under his name for {}.".format(name, stat_val, stat)
+        resp_3 = "{}...obviously".format(stat_val)
+        resp_4 = "Last I checked... {} had a {} of {}.".format(name, stat, stat_val)
+        resp_5 = "Do you not have every NBA stat since the beginning of time memorized? Well he has a {} of {}.".format(stat, stat_val)
+        resp_6 = "{} has {} of {}, impressive isn't it?".format(name, stat, stat_val)
+        resp_7 = "As we are speaking, {} has {} of {}".format(name, stat, stat_val)
+        resp_8 = "After watching hours of tapes, I came with {} of {} for {}".format(stat, stat_val, name)
+        resp_9 = "If my calculations are correct {} has {} of {} during his career".format(name, stat, stat_val)
+        resp_10 = "Couldn't you Google that? Well since we're already here: {} has {} of {}".format(name, stat, stat_val)
+        resp_list = [resp_1,
+                     resp_2,
+                     resp_3,
+                     resp_4,
+                     resp_5,
+                     resp_6,
+                     resp_7,
+                     resp_8,
+                     resp_9,
+                     resp_10]
+        return random.choice(resp_list)
+  
+    def response(self):
+        return self.generate_random_response(name, stat, stat_val)
+
+
+    def extract_name(self):
+        doc = self.nlp(self.query)
+        name = None
+        for entity in doc.ents:
+            if entity.label_ == "PERSON":
+                name = entity.text
+        if not name:
+            for entity in doc.ents:
+                if entity.label_ == "ORG":
+                    name = entity.text
+
+        return name
+    
+    def extract_stat(self):
          doc = self.nlp(self.query)
          stat = ""
          stat_final = ""
@@ -87,10 +105,10 @@ class StatNode(object):
          if(max_sim > sim_threshold):
              return str(stat_final)
          return None
-	
-	def get_player_stat(self, name, stat):
-		if stat in total_stat_map:
-			val = get_total_stat(name, stat)
-		else:
-			val = get_adv_stat(name, stat)
-		return val
+    
+    def get_player_stat(self, name, stat):
+        if stat in total_stat_map:
+            val = get_total_stat(name, stat)
+        else:
+            val = get_adv_stat(name, stat)
+        return val
