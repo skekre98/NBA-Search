36ba7ecad1bbf3a4aa86f2e52e494f7102359ee2
ritikbhatia <bhatia.ritik5@gmail.com>
2020-10-31 15:15:00 +0800

    Implement push and render for tree builder

--- lab/tree_builder.py
+++ lab/tree_builder.py
@@ -1,5 +1,7 @@
 from anytree import Node
 from anytree.exporter import DotExporter
+from graphviz import Source, render
+
 
 class TreeNode(Node):
 
@@ -17,17 +19,45 @@ class TreeBuilder(object):
         self.root = root
         self.size = 0
 
-    # Function to push to tree 
+    # Function to push to tree
     def push(self, node):
-        # TODO
-        pass
+        # pointer for the current node
+        currNode = self.root
+        while True:
+            # if value of node to be added is greater, go right
+            if node.val > currNode.val:
+                # if right child exists, go right
+                if currNode.right != None:
+                    currNode = currNode.right
+                # if no right child, add node as right child
+                else:
+                    currNode.right = node
+                    node.parent = currNode
+                    break
+            # else, go left
+            else:
+                # if left child exists, go left
+                if currNode.left != None:
+                    currNode = currNode.left
+                # if no left child, add node as left child and break
+                else:
+                    currNode.left = node
+                    node.parent = currNode
+                    break
 
-    # Function to remove node from tree 
+        return
+
+    # Function to remove node from tree
     def pop(self, name):
         # TODO
         pass
 
     # Function to export tree as png(Use DotExporter)
     def export(self, file="tree"):
-        # TODO 
-        pass
+
+        # export to dot file
+        DotExporter(self.root).to_dotfile(file+".dot")
+        Source.from_file(file+'.dot')
+        # render as png
+        render('dot', 'png', file+'.dot')
+        return
