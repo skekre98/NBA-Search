b589a47e3baaf759727e4e7a86f1d9c084e21dcc
kelaaron <kelaaron@umich.edu>
2020-12-01 13:10:55 -0500

    added model map and moved .pkl file

--- inference/models/tests/models_test.py
+++ inference/models/tests/models_test.py
@@ -4,42 +4,53 @@ sys.path.append("../../..")
 from inference.inference_network import InferenceNetwork
 import csv
 import os
+os.chdir('../../../')
 
-def test_accuracy(test_path):
-    os.system('python3 query_classifier.py') # train models, dumps into query_classifier.pkl
+def test_accuracy():
 
-    # Query classification 
-    model_file = "../query_classifier.pkl"
-    query_clf = joblib.load(model_file)
+    model_test_map = {
+        "query_classifier.py" : "inference/models/tests/test_input.csv",
+    }
+
+    for model, test_path in model_test_map.items():
 
 
-    correct = 0
-    total = 0
+        os.system('python3 ' + model) # train models, dumps into model .pkl file
 
-    with open(test_path) as inputfile:
-        readCSV = csv.reader(inputfile, delimiter=',')
-        for row in readCSV:
-            query = row[0]
-            node_type = query_clf.predict([query.lower()])[0]
-            total += 1
-            
-            if node_type.lower() == row[1].lower():
-                correct += 1
-            else:
-                print("ON query: " + query + ", PREDICTED " + node_type.lower() + " INSTEAD OF " + row[1].lower())
+        model_name = model[:len(model) - 3]
 
-    
-    print("PREDICTED " + str(correct) + " OUT OF " + str(total) + ", OR " + str(correct/total) + "%")
+        # Query classification 
+        model_file = "inference/models/classifiers/" + model_name + ".pkl"
+        query_clf = joblib.load(model_file)
+
+
+        correct = 0
+        total = 0
+
+        with open(test_path) as inputfile:
+            readCSV = csv.reader(inputfile, delimiter=',')
+            for row in readCSV:
+                query = row[0]
+                node_type = query_clf.predict([query.lower()])[0]
+                total += 1
+                
+                if node_type.lower() == row[1].lower():
+                    correct += 1
+                else:
+                    print("ON query: " + query + ", PREDICTED " + node_type.lower() + " INSTEAD OF " + row[1].lower())
+
+        
+        print("PREDICTED " + str(correct) + " OUT OF " + str(total) + ", OR " + str(correct/total) + "%")
 
     return "DONE"
 
 def models_test():
-    # USAGE: python3 models_test.py MODEL TESTCRITERIA PATH_TO_TEST_CASE
-    if len(sys.argv)<3:
+    # USAGE: python3 models_test.py MODEL TESTCRITERIA
+    if len(sys.argv)<2:
         print("ERROR: Missing Arguments")
-        print("USAGE: python3 models_test.py MODEL TESTCRITERIA PATH_TO_TEST_CASE")
+        print("USAGE: python3 models_test.py MODEL TESTCRITERIA")
     elif sys.argv[1] == "classifier":
         if sys.argv[2] == "accuracy":
-            test_accuracy(sys.argv[3])
+            test_accuracy()
 
 models_test()
