12085264d01fa7741088b2afb3a39f9fe08e2dfd
abhijeetsharma200 <abhijeetsharma2002@gmail.com>
2020-12-25 00:46:01 +0530

    Removed usage of os.system() and used modules instead

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import os
 import sys
+import unittest
+import tests.flask_tests as flask_tests
+import tests.module_tests as module_tests
+import tests.inference_tests as inference_tests
+from app import app as application
+import visualize
 
 # Function to print help messages
 def printHelp():
@@ -10,21 +16,24 @@ def printHelp():
     print("    data: data visualization for training sets")
 
 def main():
-    
+
     if len(sys.argv)<=1:
         print("ERROR: Missing Arguments")
         printHelp()
     elif sys.argv[1] == "test":
         print("Flask Tests")
-        os.system('python3 -W ignore -m unittest tests/flask_tests.py')
+        suite = unittest.TestLoader().loadTestsFromModule(flask_tests)
+        unittest.TextTestRunner(verbosity=0).run(suite)
         print("Module Tests")
-        os.system('python3 -W ignore -m unittest tests/module_tests.py')
+        suite = unittest.TestLoader().loadTestsFromModule(module_tests)
+        unittest.TextTestRunner(verbosity=0).run(suite)
         print("Inference Tests")
-        os.system('python3 -W ignore -m unittest tests/inference_tests.py')
+        suite = unittest.TestLoader().loadTestsFromModule(inference_tests)
+        unittest.TextTestRunner(verbosity=0).run(suite)
     elif sys.argv[1] == "run":
-        os.system('python3 app.py')
+        application.run()
     elif sys.argv[1] == "data":
-        os.system('python3 visualize.py')
+        visualize.main()
     else:
         print("ERROR: Invalid Arguments")
         printHelp()
