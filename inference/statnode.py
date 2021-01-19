973ea346c224e74d1ca4a7c26af466dbe5ad0277
Florent18570 <71646182+Florent18570@users.noreply.github.com>
2021-01-19 08:00:57 +0100

    Update statnode.py

--- inference/statnode.py
+++ inference/statnode.py
@@ -150,65 +150,45 @@ class StatNode(object):
         if stat_final != "":
             return stat_final
 
+        # if no exact match was found, use fuzzy string matching to find a close match
+        # create lists out of stat maps' keys
+        stat_map1 = total_stat_map.keys()
+        stat_map2 = adv_stat_map.keys()
 
-            # if no exact match was found, we need to check similarity between stats and user query
-        stat = self.nlp(stat)
-        for entry in total_stat_map:
-            entry = self.nlp(entry)
-            sim = stat.similarity(entry)
-            if max_sim < sim:
-                max_sim = sim
-                stat_final = entry  # using stat_final because original variable stat is used to check similarity, so we can not change it
+        # extract fuzzy matches from total and adv stat maps, compare which is closer and return
+        # 'doc' gives better results than 'stat', worth continuing to look into
+        stat_total, ratio_total = process.extractOne(str(doc), stat_map1)
+        stat_adv, ratio_adv = process.extractOne(str(doc), stat_map2)
+        # if neither ratio is higher than the similarity threshold, return None
+        if ratio_total < sim_threshold and ratio_adv < sim_threshold:
+            return None
+        elif ratio_total > ratio_adv:
+            return stat_total
+        else:
+            return stat_adv
+    """
+      Function to get player statistics
+    
+        Parameters
+        ----------
+        self : none
+        stat : string
+          The stat that is being compared to stat_val
+        name : string
+          NBA player for stat retrieval    
+    
+        Returns
+        -------
+        val : string
+          statistic value
+    """
+    def get_player_stat(self, name, stat):
+        if stat in total_stat_map:
+            val = get_total_stat(name, stat)
+        else:
+            val = get_adv_stat(name, stat)
+        return val
 
-        for entry in adv_stat_map:
-            entry = self.nlp(entry)
-            sim = stat.similarity(entry)
-            if max_sim < sim:
-                max_sim = sim
-                stat_final = entry
-
-        # returning extracted stat if similarity exceeds minimum threshold
-        if (max_sim > sim_threshold):
-            return str(stat_final)
-        return None
 
 
-"""
-  Function to get player statistics
 
-    Parameters
-    ----------
-    self : none
-    stat : string
-      The stat that is being compared to stat_val
-    name : string
-      NBA player for stat retrieval    
-
-    Returns
-    -------
-    val : string
-      statistic value
-"""
-# if no exact match was found, use fuzzy string matching to find a close match
-# create lists out of stat maps' keys
-stat_map1 = total_stat_map.keys()
-stat_map2 = adv_stat_map.keys()
-
-# extract fuzzy matches from total and adv stat maps, compare which is closer and return
-# 'doc' gives better results than 'stat', worth continuing to look into
-stat_total, ratio_total = process.extractOne(str(doc), stat_map1)
-stat_adv, ratio_adv = process.extractOne(str(doc), stat_map2)
-# if neither ratio is higher than the similarity threshold, return None
-if ratio_total < sim_threshold and ratio_adv < sim_threshold:
-    return None
-elif ratio_total > ratio_adv:
-    return stat_total
-else:
-    return stat_adv
-
-def get_player_stat(self, name, stat):
-    if stat in total_stat_map:
-        val = get_total_stat(name, stat)
-    else:
-        val = get_adv_stat(name, stat)
-    return val
