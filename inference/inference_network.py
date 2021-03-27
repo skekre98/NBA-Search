f123b75373dcfe0ce9f2abc1ad5e04fcea3895e5
Florent18570 <71646182+Florent18570@users.noreply.github.com>
2021-03-27 20:28:00 +0100

    Improve documentation in Inference Network class

--- inference/inference_network.py
+++ inference/inference_network.py
@@ -7,17 +7,27 @@ from inference.statnode import StatNode
 from inference.infonode import InfoNode
 from data.text_data import unsure, non_nba
 
+"""
+a class used to represent InferenceNetwork
 
+parameters
+-------
+object: string
+return
+
+-------
+n/a
+"""
 class InferenceNetwork(object):
 
     def __init__(self, query):
         self.query = query
 
-        # Query classification 
+        # Query classification
         model_file = "inference/models/query_classifier.pkl"
         query_clf = joblib.load(model_file)
         self.node_type = query_clf.predict([query.lower()])[0]
-    
+
     def response(self):
         if self.node_type == "info":
             node = InfoNode()
@@ -28,11 +38,11 @@ class InferenceNetwork(object):
                 return unsure
             elif flag == -1:
                 return non_nba
-            
+
             # Query is definitely NBA related
             if self.node_type == "rank":
                 node = RankNode()
-            
+
             if self.node_type == "stat":
                 node = StatNode()
 
